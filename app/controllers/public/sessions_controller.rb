7366307adf31bc717ea95aa8cccc335791115124
tayboy217 <tayboy217@icloud.com>
2021-12-05 14:07:51 +0000

    [Add]Devise

---
+++ app/controllers/public/sessions_controller.rb
@@ -0,0 +1,38 @@
+# frozen_string_literal: true
+
+class Public::SessionsController < Devise::SessionsController
+  
+  #ゲストログイン
+  def new_guest
+    user = User.find(1)
+    user.update(email: 'guest@example.com',name: 'ゲストユーザー') do |user|
+      user.password = SecureRandom.urlsafe_base64
+    end
+    sign_in user
+    redirect_to root_path
+  end
+  
+  # before_action :configure_sign_in_params, only: [:create]
+
+  # GET /resource/sign_in
+  # def new
+  #   super
+  # end
+
+  # POST /resource/sign_in
+  # def create
+  #   super
+  # end
+
+  # DELETE /resource/sign_out
+  # def destroy
+  #   super
+  # end
+
+  # protected
+
+  # If you have extra params to permit, append them to the sanitizer.
+  # def configure_sign_in_params
+  #   devise_parameter_sanitizer.permit(:sign_in, keys: [:attribute])
+  # end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,21 @@
 Rails.application.routes.draw do
+  #デバイスUser側
+  devise_for :users,skip: [:passwords,], controllers: {
+  registrations: "public/registrations",
+  sessions: 'public/sessions'
+}
+  
+  #ゲストログイン
+  devise_scope :user do
+    post 'users/guest_sign_in', to: 'users/sessions#new_guest'
+  end
+  
+  #デバイスAdmin側
+  devise_for :admins, skip: [:registrations, :passwords] ,controllers: {
+  sessions: "admin/sessions"
+}
+  
+ root to: 'home#top'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
