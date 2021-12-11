de4d454fb8b24fb1890eef1c1e407126dec52cba
tayboy217 <tayboy217@icloud.com>
2021-12-11 04:48:46 +0000

    [Update]TaskNewPost

--- app/controllers/public/tasks_controller.rb
+++ app/controllers/public/tasks_controller.rb
@@ -1,6 +1,7 @@
 class Public::TasksController < ApplicationController
 
   def new
+    @task = Task.new
   end
 
   def index
@@ -10,6 +11,13 @@ class Public::TasksController < ApplicationController
   end
 
   def create
+     @task = Task.new(task_params)
+     @task.user_id = current_user.id
+  if @task.save
+     redirect_to new_task_path
+  else
+     render :new
+  end
   end
 
   def edit
@@ -23,7 +31,7 @@ class Public::TasksController < ApplicationController
 
   private
   def task_params
-    params.require(:note).permit(:body, :post_date)
+    params.require(:task).permit(:body, :post_date)
   end
 
 end
