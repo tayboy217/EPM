0adcb1c216580317d3407464d854079b738fde97
tayboy217 <tayboy217@icloud.com>
2021-12-17 19:06:41 +0000

    [Update]Layout

--- app/controllers/public/users_controller.rb
+++ app/controllers/public/users_controller.rb
@@ -1,30 +1,30 @@
 class Public::UsersController < ApplicationController
-  
+
   def show
     @user = current_user
   end
-  
+
   def edit
     @user = User.find(params[:id])
   end
-  
+
   def update
-    @user = User.find(prams[:id])
+    @user = User.find(params[:id])
   if@user.update(user_params)
     redirect_to users_path
-  else 
+  else
     render :edit
   end
-  end 
-  
+  end
+
   def quit
     @user = current_user
-  end 
-  
-  
+  end
+
+
   private
   def user_params
     params.require(:user).permit(:email, :name)
   end
-  
+
 end
